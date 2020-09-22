bfb079f6774f0628b8fc3a23e925363326189f51
Nikolay Mikhaylov <sonny@milton.pro>
2020-09-22 14:10:45 +0300

    Add frontend

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,8 +9,81 @@
 import '../css/app.scss';
 import 'materialize-css'
 
+
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
 M.AutoInit();
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+const poll = document.querySelector('.poll');
+const questions = [...poll.querySelectorAll('.question')];
+const lastQuestion = questions.slice(-1)[0];
+const firstQuestion = questions[0];
+
+const buttons = {
+    next: document.querySelector('.js-question-next'),
+    prev: document.querySelector('.js-question-prev'),
+    submit: document.querySelector('.js-poll-submit'),
+}
+
+function validateAnswer(question) {
+    const {required, fieldType} = question.dataset;
+
+    if (!required) {
+        return true;
+    }
+
+    if (fieldType === 'choice') {
+        return question.querySelectorAll('input:checked').length !== 0;
+    }
+
+    const field = question.querySelector('input');
+
+    return field.value.length !== 0;
+}
+
+buttons.next.addEventListener('click', () => {
+    const activeQuestionIndex = questions.findIndex(question => question.classList.contains('active'));
+    const activeQuestion = questions[activeQuestionIndex];
+
+    if (!validateAnswer(activeQuestion)) {
+        M.toast({'html': 'Invalid answer'})
+        return;
+    }
+
+    const nextQuestion = questions[activeQuestionIndex + 1];
+
+    activeQuestion.classList.remove('active');
+    nextQuestion.classList.add('active');
+
+    if (nextQuestion === lastQuestion) {
+        buttons.next.classList.add('hide');
+        buttons.submit.classList.remove('hide');
+    }
+
+    buttons.prev.classList.remove('hide');
+});
+
+buttons.prev.addEventListener('click', () => {
+    const activeQuestionIndex = questions.findIndex(question => question.classList.contains('active'));
+    const activeQuestion = questions[activeQuestionIndex];
+    const prevQuestion = questions[activeQuestionIndex - 1];
+
+    activeQuestion.classList.remove('active');
+    prevQuestion.classList.add('active');
+
+    if (prevQuestion === firstQuestion) {
+        buttons.prev.classList.add('hide');
+    }
+
+    buttons.next.classList.remove('hide')
+    buttons.submit.classList.add('hide');
+})
+
+
+buttons.submit.addEventListener('click', e => {
+    if (!validateAnswer(lastQuestion)) {
+        e.preventDefault();
+        M.toast({'html': 'Invalid answer'})
+    }
+});
